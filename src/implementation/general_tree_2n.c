23a73148eb83f088e9548b9760185061ae72f594
bernardoarantes <bernardoarantes2006@gmail.com>
2026-03-12 19:27:36 -0300

    Empty commit for testing

--- src/implementation/general_tree_2n.c
+++ src/implementation/general_tree_2n.c
@@ -2,6 +2,6 @@
 // Graphviz library) general trees as a 2n bit array
 // and  builds up a general tree from that array
 
-Utils: 
-    "https://graphviz.org/documentation/",
-    "General-Tree-Encode-2n-bits.jpg",
+// Utils: 
+//     "https://graphviz.org/documentation/",
+//     "General-Tree-Encode-2n-bits.jpg",
