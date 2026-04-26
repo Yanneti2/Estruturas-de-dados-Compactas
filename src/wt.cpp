954cc11e579f1f99caf08a0286f78e23abcad006
Victor Marques <vhmar2005@gmail.com>
2026-04-25 21:02:33 -0300

    Wavelet Tree implementation

---
+++ src/wt.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <cstdlib>
+#include "wt.h"
+#include "..\include\bitvector.h"
+#include <map>
+#include <algorithm>
+
+using namespace std;
+
+// lembrar de fazer a implicita dps
+
+map<char, bool> Hashing(string S) {
+    map<char, bool> chars = {};
+    for(uint32_t i = 0; i < S.size(); i++)
+        if(chars.find(S[i]) == chars.end()) 
+            chars.insert({S[i], true});
+    return chars;
+}
+
+WaveletTree::WaveletTree(string S, WaveletTree* dad) {
+    this->d = dad;
+    map<char, bool> chars = Hashing(S);
+    string alpha;
+    for (auto i = chars.begin(); i != chars.end(); i++) {
+        alpha += i->first;                         
+    }
+
+    if (alpha.size() == 1){
+        this->l = NULL;
+        this->r = NULL;
+        this->d = dad;
+        return;
+    }
+
+    uint32_t len = alpha.size() - 1;
+    sort(alpha.begin(), alpha.end());
+    bitVector vector = bitVector((unsigned long) ((S.size() + NBITS - 1)/NBITS), 2);
+    uint32_t mid = len / 2;
+    string LSS, RSS;
+
+    for (uint32_t i = 0; i < S.size(); i++) {
+        if (alpha[mid] >= S[i]) {
+            vector.append0();
+            LSS += S[i];
+        }
+        else {
+            vector.append1();
+            RSS += S[i];
+        }
+    }
+    this->freq = vector;
+    if(LSS.size()) {
+        this->l = &WaveletTree(LSS, this);
+    }
+    if(RSS.size()) {
+        this->r = &WaveletTree(RSS, this);
+    }
+}
+
+
+WaveletTree::~WaveletTree() {
+    
+}
+
+unsigned long long WaveletTree::rank() {
+}
+
+unsigned long long WaveletTree::select() {
+}
