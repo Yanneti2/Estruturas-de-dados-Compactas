b77057ba9b678e84ab207626c68d206cce67f8de
bernardoarantes <bernardoarantes2006@gmail.com>
2026-05-05 17:01:06 -0300

    Created test file for RRR.

---
+++ tests/RRR-tester.cpp
@@ -0,0 +1,43 @@
+#include "../include/RRR.h" 
+#include <iostream>
+#include <iomanip>
+#include <chrono>
+#include <math.h>
+#include <cstdlib>
+#include <assert.h>
+
+int main(void) {
+    srand(time(0));
+    cout << "\"Size\";\"Time\"\n";
+    for (long long unsigned size = 1000; size < 10000; size *= 10) {
+        bitVector B(64, 2.0);
+        int order = log10(size);
+        // cout << "Order: " << order << endl;
+
+        for (TYPE i = 0; i < size; i++) {
+            if (rand() % 2 == 0) {
+                B.append1();
+            } else {
+                B.append0();
+            }
+        }
+        
+
+        auto start = std::chrono::high_resolution_clock::now();
+        RRR R(&B);
+        auto end = std::chrono::high_resolution_clock::now();
+        std::chrono::duration<double, std::nano> ns_time = (end - start);
+        // cout << std::fixed << std::setprecision(5) << "Construction time: " << ns_time.count() << "ns\n";
+        
+        B.print();
+        R.print();
+        unsigned long long index = rand();
+        start = std::chrono::high_resolution_clock::now();
+        R.rank1(index);
+        end = std::chrono::high_resolution_clock::now();
+        ns_time = end - start;
+        cout << size << ";" << ns_time.count() << "\n";
+        
+        
+    }
+}
